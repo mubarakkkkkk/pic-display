b27b5b8235da555dc855cbfa7fb51649b0c34725
mubarakkkkkk <abiolamubarakolamide05@gmail.com>
2024-08-24 00:49:07 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className="container">
+        <button className='firstPicture'>Display</button>
+        <button className='seoundPicture'>Display</button>
+        <button className='thirdPicture'>Display</button>
+      </div>
+    </>
+  )
+}
+
+export default App
